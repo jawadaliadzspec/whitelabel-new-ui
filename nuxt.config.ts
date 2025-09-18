6a78f146748ee3c044cd8a9255d2068cd60160f3
Jawad Ali <jawad.ali@adzspec.com>
2025-09-18 16:53:27 +0500

    nuxt config update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineNuxtConfig({
-  app: { baseURL: '/' },
+  app: { baseURL: '/app/' },
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
